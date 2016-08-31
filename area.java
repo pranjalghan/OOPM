40d0a768b2fb8226457bd24ef13050ff4284ce35
Tanuj Jain <tanujjain244@yahoo.com>
2016-08-31 22:09:31 +0530

    Use Indentation and remove whitespaces

--- area.java
+++ area.java
@@ -1,28 +1,25 @@
 public class area
 {
-float l,b;
-
-void area(int l,int b)
-{
-float area;
-area=l*b;
-System.out.println("AREA="+area);
-}
-void area(double l,double b)
-{
-double area;
-area=l*b;
-System.out.println("AREA="+area);
-}
+ float l,b;
+ void area(int l,int b)
+ {
+  float area;
+  area=l*b;
+  System.out.println("AREA="+area);
+ }
+ void area(double l,double b)
+ {
+ double area;
+ area=l*b;
+ System.out.println("AREA="+area);
+ }
 }
 class demo
 {
-public static void main(String args[])
-{
-area x=new area();
-
-
-x.area(2,3);
-x.area(2.14,3.16);
-}
+ public static void main(String args[])
+ {
+  area x=new area();
+  x.area(2,3);
+  x.area(2.14,3.16);
+ }
 }
